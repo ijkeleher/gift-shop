f97653bac25ba701d68af47d25f4f02ce33e2ffa
Cass <s3660446@student.rmit.edu.au>
2018-07-31 01:28:33 +1000

    Changed footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Footer = () => (
   <footer>
-    Created by <a href="https://www.linkedin.com/in/jeffersonribeiro123/" rel="noopener noreferrer" target="_blank" title="Linkedin Profile">Jefferson Ribeiro</a>
+    Created by Claudia, Inci, Jack and Cassandra of Blue Team 
   </footer>
 );
 

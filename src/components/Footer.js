e43e7e908d5aab2824f1d456123d42a13faac17e
Inci <inci90@users.noreply.github.com>
2018-07-31 12:05:17 +1000

    minor front-end changes -> typos in footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,11 +3,7 @@ import React from 'react';
 
 const Footer = () => (
   <footer>
-<<<<<<< HEAD
-    Created by Claudia, Inci, Jack and Cassandra of Blue Team 
-=======
-    Created by Claudia, Insi, Jack and Cassandra of Blue Team 
->>>>>>> d076a0dc3eae9d8fd321ead4779d00c747fc6bc5
+    Created by Claudia, Inci, Jack and Cassandra of Blue Team
   </footer>
 );
 

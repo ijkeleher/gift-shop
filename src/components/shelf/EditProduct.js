f98de539a128a1ca8bc101ca505c5c47b536092d
claudia <s3668061@student.rmit.edu.au>
2018-08-19 16:20:19 +1000

    edit item button works

---
+++ src/components/shelf/EditProduct.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+class EditProduct extends Component {
+
+  editItem = () => {
+    
+  }
+
+
+  render() {
+
+    return(
+      <div>
+        <p>Does this even work???</p>
+      </div>
+    );
+
+  }
+}
+
+export default EditProduct;
--- src/components/shelf/Product.js
+++ src/components/shelf/Product.js
@@ -1,56 +1,84 @@
 import React from 'react';
 import PropTypes from "prop-types";
+import EditProduct from "./EditProduct";
 
 import Thumb from '../Thumb';
 
 import util from '../../util';
 
+class Product extends React.Component {
 
-const Product = (props) => {
-  const product = props.product;
-  // Um componente de input pode alterar a quantidade no futuro
-  product.quantity = 1;
+  state = {
+    showEditMenu: false
+  }  
 
-  let formattedPrice = util.formatPrice(product.price, product.currencyId);
-  
-  let productInstallment;
-  
-  if(!!product.installments) {
-    const installmentPrice = (product.price / product.installments);
+  editItemMenu = () => {
+    if(this.state.showEditMenu === false){
+      this.setState({
+        showEditMenu: true 
+      });
+    }
+    else{
+      this.setState({
+        showEditMenu: false 
+      });
+    }
 
-    productInstallment = (
-      <div className="installment">
-        <span>or {product.installments} x</span><b> {product.currencyFormat} {util.formatPrice(installmentPrice, product.currencyId)}</b>
+  }
+
+  render(){
+    
+    const product = this.props.product;
+    // Um componente de input pode alterar a quantidade no futuro
+    product.quantity = 1;
+
+    let formattedPrice = util.formatPrice(this.props.product.price, this.props.product.currencyId);
+    
+    let productInstallment;
+    
+    if(!!product.installments) {
+      const installmentPrice = (product.price / product.installments);
+
+      productInstallment = (
+        <div className="installment">
+          <span>or {product.installments} x</span><b> {product.currencyFormat} {util.formatPrice(installmentPrice, product.currencyId)}</b>
+        </div>
+      );
+    }
+    return (
+      <div className="shelf-item" data-sku={this.props.product.sku}>
+        {this.props.product.isFreeShipping && 
+          <div className="shelf-stopper">Free shipping</div>
+        }
+        <Thumb
+          classes="shelf-item__thumb"
+          src={require(`../../static/products/${this.props.product.sku}_1.jpg`)}
+          alt={this.props.product.title}
+        />
+        <p className="shelf-item__title">{this.props.product.title}</p>
+        <div className="shelf-item__price">
+          <div className="val"><small>{this.props.product.currencyFormat}</small>
+            <b>
+              {formattedPrice.substr(0, formattedPrice.length - 3)}
+            </b>
+            <span>
+              {formattedPrice.substr(formattedPrice.length - 3, 3)}
+            </span>
+          </div>
+          {productInstallment}
+        </div>
+        <div onClick = {this.editItemMenu}>Edit</div>
+        {this.state.showEditMenu ?
+            <EditProduct /> :
+            null
+          }
+        <div onClick={() => this.props.addProduct(this.props.product)} className="shelf-item__buy-btn">Add to cart</div>
       </div>
     );
   }
 
-  return (
-    <div className="shelf-item" data-sku={product.sku}>
-      {product.isFreeShipping && 
-        <div className="shelf-stopper">Free shipping</div>
-      }
-      <Thumb
-        classes="shelf-item__thumb"
-        src={require(`../../static/products/${product.sku}_1.jpg`)}
-        alt={product.title}
-      />
-      <p className="shelf-item__title">{product.title}</p>
-      <div className="shelf-item__price">
-        <div className="val"><small>{product.currencyFormat}</small>
-          <b>
-            {formattedPrice.substr(0, formattedPrice.length - 3)}
-          </b>
-          <span>
-            {formattedPrice.substr(formattedPrice.length - 3, 3)}
-          </span>
-        </div>
-        {productInstallment}
-      </div>
-      <div onClick={() => props.addProduct(product)} className="shelf-item__buy-btn">Add to cart</div>
-    </div>
-  );
 }
+
 
 
 Product.propTypes = {

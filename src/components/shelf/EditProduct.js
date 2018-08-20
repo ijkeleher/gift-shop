98ab44c35128f7ad059cc3b99287229e2e8fb966
claudia <s3668061@student.rmit.edu.au>
2018-08-20 20:50:44 +1000

    edit json working with dummy jason fred

--- src/components/shelf/EditProduct.js
+++ src/components/shelf/EditProduct.js
@@ -3,8 +3,27 @@ import React, { Component } from 'react';
 
 class EditProduct extends Component {
 
-  editItem = () => {
-    
+  state = {
+    data: null
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8001/api/products")
+      .then(response => response.json())
+      .then(data => this.setState({ data }));
+  }
+
+  editItem = (e) => {
+    e.preventDefault();
+    console.log(this.state.data);
+    var userWord = document.getElementById("word").value;
+    var userNum = document.getElementById("number").value;
+
+    fetch(`http://localhost:8001/api/products/edit/${userWord}/${userNum}?`);
+  }
+
+  gotData = (data) =>{
+    console.log(data)
   }
 
 
@@ -12,7 +31,11 @@ class EditProduct extends Component {
 
     return(
       <div>
-        <p>Does this even work???</p>
+        <form onSubmit = {this.editItem}>
+          <input type="text" id="word"/>
+          <input type="text" id="number"/>
+          <button>Go</button>
+        </form>
       </div>
     );
 

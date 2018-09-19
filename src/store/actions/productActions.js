2dfed545f932b8a73de5079747d5b21680ce4f95
Inci <inci90@users.noreply.github.com>
2018-09-19 15:25:54 +1000

    updated json link

--- src/store/actions/productActions.js
+++ src/store/actions/productActions.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 // const productsAPI = "https://react-shopping-cart-67954.firebaseio.com/products.json";
 //const productsAPI = "http://localhost:8001/api/products";
 
-const productsAPI = "https://api.myjson.com/bins/15qhx0";
+const productsAPI = "https://storage.googleapis.com/rmit-giftshop/products.json";
 
 const compare = {
   'lowestprice': (a, b) => {
